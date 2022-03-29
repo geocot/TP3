adffa2d7198f8d228b8989a35eab395cd7a957a4
geocot <martin.couture@uqtr.ca>
2022-03-28 21:40:26 -0400

    Ajout classe employé

---
+++ GestionEmployes/src/main/java/com/example/tp3/Employe.java
@@ -0,0 +1,67 @@
+package com.example.tp3;
+
+import java.util.Date;
+
+public class Employe {
+    private String nomUsager;
+    private String nomFamille;
+    private String prenom;
+    private Date dateDebut;
+    private Date dateFin;
+    private String noEmploye;
+
+    public Employe(String nomUsager, String nomFamille, String prenom, String noEmploye) {
+        this.nomUsager = nomUsager;
+        this.nomFamille = nomFamille;
+        this.prenom = prenom;
+        this.noEmploye = noEmploye;
+    }
+
+    public String getNomUsager() {
+        return nomUsager;
+    }
+
+    public void setNomUsager(String nomUsager) {
+        this.nomUsager = nomUsager;
+    }
+
+    public String getNomFamille() {
+        return nomFamille;
+    }
+
+    public void setNomFamille(String nomFamille) {
+        this.nomFamille = nomFamille;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public Date getDateDebut() {
+        return dateDebut;
+    }
+
+    public void setDateDebut(Date dateDebut) {
+        this.dateDebut = dateDebut;
+    }
+
+    public Date getDateFin() {
+        return dateFin;
+    }
+
+    public void setDateFin(Date dateFin) {
+        this.dateFin = dateFin;
+    }
+
+    public String getNoEmploye() {
+        return noEmploye;
+    }
+
+    public void setNoEmploye(String noEmploye) {
+        this.noEmploye = noEmploye;
+    }
+}
