77f4d36f06a20a69bd86b3fb3a15297cdb6e3731
geocot <martin.couture@uqtr.ca>
2022-04-06 18:50:18 -0400

    Avancement

--- GestionEmployes/src/main/java/com/example/tp3/GestionEmployesApplication.java
+++ GestionEmployes/src/main/java/com/example/tp3/GestionEmployesApplication.java
@@ -11,7 +11,8 @@ import java.io.IOException;
 public class GestionEmployesApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(GestionEmployesApplication.class.getResource("gestionEmployes.fxml"));
+        //FXMLLoader fxmlLoader = new FXMLLoader(GestionEmployesApplication.class.getResource("gestionEmployes.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(GestionEmployesApplication.class.getResource("RechercheActivites.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Gestion des employés");
 
