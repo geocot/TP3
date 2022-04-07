da5c51d999d4a81766d4419f39b6df1ca7f8d7a3
geocot <martin.couture@uqtr.ca>
2022-04-06 22:35:25 -0400

    Ajout du hub, reste à terminer le code pour revenir en arrière

--- GestionEmployes/src/main/java/com/example/tp3/GestionEmployesApplication.java
+++ GestionEmployes/src/main/java/com/example/tp3/GestionEmployesApplication.java
@@ -2,6 +2,7 @@ package com.example.tp3;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -9,18 +10,27 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class GestionEmployesApplication extends Application {
+    /* Ancienne méthode
     @Override
     public void start(Stage stage) throws IOException {
         //FXMLLoader fxmlLoader = new FXMLLoader(GestionEmployesApplication.class.getResource("gestionEmployes.fxml"));
-        //stage.setTitle("Gestion des employés");
-        stage.setTitle("Recherche d'activités");
-        FXMLLoader fxmlLoader = new FXMLLoader(GestionEmployesApplication.class.getResource("RechercheActivites.fxml"));
+        //FXMLLoader fxmlLoader = new FXMLLoader(GestionEmployesApplication.class.getResource("RechercheActivites.fxml"));
+        stage.setTitle("AquaForme");
+        FXMLLoader fxmlLoader = new FXMLLoader(GestionEmployesApplication.class.getResource("hub.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
-
-
+        stage.setScene(scene);
+        stage.show();
+    }*/
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("hub.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("AquaForme");
         stage.setScene(scene);
         stage.show();
     }
+
+
 
     public static void main(String[] args) {
         launch();
